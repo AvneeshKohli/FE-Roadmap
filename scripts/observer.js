500b750078926303bf6dedd65d75899a4cc5fb29
AvneeshKohli <avneeshkohli3@gmail.com>
2020-11-02 00:39:51 +0530

    added images and UI improvements

--- scripts/observer.js
+++ scripts/observer.js
@@ -1,19 +1,26 @@
 //const wrappers = document.querySelector('.wrapper-odd');
-const wrappers = document.querySelectorAll('.wrapper-odd')
-const wrappersEven = document.querySelectorAll('.wrapper-even')
+const wrappers = document.querySelectorAll('.main-topic-left')
+const wrappersEven = document.querySelectorAll('.main-topic-right')
+const topicContent = document.querySelectorAll('.topic-name')
+const img = document.querySelectorAll('.img-bg')
 const options = {
     root: null,
     threshold: 0.2,
     rootMargin: "-75px 0px"
 };
 
-const observer = new IntersectionObserver(function(entries,observer) {
+const observer = new IntersectionObserver(function (entries, observer) {
     entries.forEach(entry => {
-        if(!entry.isIntersecting) {
+        if (!entry.isIntersecting) {
             return;
         }
-        console.log(entry.target);
-        entry.target.classList.add("bring-into-view")
+        if (entry.target.tagName === "IMG") {
+
+            entry.target.classList.add("image-in-view")
+            console.log('image in view')
+        } else { 
+            entry.target.classList.add("bring-into-view") 
+        }
         observer.unobserve(entry.target)
     })
 }, options);
@@ -21,5 +28,12 @@ wrappersEven.forEach(wrapper => {
     observer.observe(wrapper)
 })
 wrappers.forEach(wrapper => {
+    observer.observe(wrapper);
+})
+topicContent.forEach(wrapper => {
+    observer.observe(wrapper);
+})
+
+img.forEach(wrapper => {
     observer.observe(wrapper);
 })
